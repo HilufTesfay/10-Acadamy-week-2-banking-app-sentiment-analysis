6cbffa8c77d6ee99e1262760e7ebbb8526ebef76
hiluf <hiluftesfay43@gmail.com>
2025-06-08 17:59:12 +0300

    bug fixed

--- scripts/main.py
+++ scripts/main.py
@@ -2,7 +2,7 @@ import src.data.collector as dc
 
 def main():
     """
-    Main function to collect data from the Google Play Store.
+    function to collect data from the Google Play Store.
     """
     dc.collect_data()
 
